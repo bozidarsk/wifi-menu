452a510d6b4bf5cf43676d3bf682190e36a7f9b7
bozidarsk <bozidarkabahcijski@gmail.com>
2023-08-19 15:42:49 +0300

    Improved build script.

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 # runtime: mono, gtk-sharp-3, gtk-layer-shell
 
 mcs '-recurse:*.cs' -pkg:gtk-sharp-3.0 -out:wifi-menu
-chmod 755 ./wifi-menu
-sudo chown root:root ./wifi-menu
-sudo mv ./wifi-menu /usr/local/bin
+if [[ $? -eq 0 ]]; then
+	chmod 755 ./wifi-menu
+	sudo chown root:root ./wifi-menu
+	sudo mv ./wifi-menu /usr/local/bin
+else
+	echo "Build failed."
+	exit 1
+fi
